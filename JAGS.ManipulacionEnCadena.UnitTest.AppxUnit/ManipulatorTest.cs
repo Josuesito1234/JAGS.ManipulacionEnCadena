b68ec99628c7cd72d2310c780dd57e76f8bff16c
Josuesito1234 <sosajosue2006@outlook.com>
2024-10-10 22:43:34 -0600

    Testeo de pruebas unitarias exitosa

---
+++ JAGS.ManipulacionEnCadena.UnitTest.AppxUnit/ManipulatorTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAGS.ManipulacionEnCadena.UnitTest.AppxUnit
+{
+    public class ManipulatorTest
+    {
+        [Fact]
+        public void ReverseString_ShouldReturnReveseString()
+        {
+            Manipulator manipulator = new Manipulator();
+
+            var input = "Josue";
+            var esperado = "eusoJ";
+
+            var result = manipulator.ReverseString(input);
+
+            Assert.Equal(esperado, result);
+
+        }
+        [Fact]
+        public void RemoveSpaces_ShouldRemoveAllSpaces()
+        {
+            Manipulator manipulator = new Manipulator();
+
+            var input = "J O S U E";
+            var esperado = "JOSUE";
+
+            var result = manipulator.RemoveSpaces(input);
+
+            Assert.Equal(esperado, result);
+        }
+
+
+    }
+}
